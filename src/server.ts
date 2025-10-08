3ddeb8610dc10002f115704430ac5776b0b8d827
Toha Hossain <tohahpro@gmail.com>
2025-10-08 19:01:13 +0600

    server setup and prisma user schema model setup complete

---
+++ src/server.ts
@@ -0,0 +1,81 @@
+import http, { Server } from "http";
+
+import dotenv from "dotenv";
+// import { prisma } from "./config/db";
+import app from "./app";
+
+dotenv.config();
+
+let server: Server | null = null;
+
+
+async function connectToDB() {
+  try {
+    // await prisma.$connect()
+    console.log("DB connect successfully")
+  } catch (error) {
+    console.log("DB connect failed")
+    process.exit(1);
+  }
+}
+
+
+async function startServer() {
+  try {
+    await connectToDB()     
+    server = http.createServer(app);
+    server.listen(process.env.PORT, () => {
+      console.log(`🚀 Server is running on port ${process.env.PORT}`);
+    });
+
+    handleProcessEvents();
+  } catch (error) {
+    console.error("❌ Error during server startup:", error);
+    process.exit(1);
+  }
+}
+
+/*
+ * Gracefully shutdown the server and close database connections.
+ * @param {string} signal - The termination signal received.
+ */
+async function gracefulShutdown(signal: string) {
+  console.warn(`🔄 Received ${signal}, shutting down gracefully...`);
+
+  if (server) {
+    server.close(async () => {
+      console.log("✅ HTTP server closed.");
+
+      try {
+        console.log("Server shutdown complete.");
+      } catch (error) {
+        console.error("❌ Error during shutdown:", error);
+      }
+
+      process.exit(0);
+    });
+  } else {
+    process.exit(0);
+  }
+}
+
+/**
+ * Handle system signals and unexpected errors.
+ */
+function handleProcessEvents() {
+  process.on("SIGTERM", () => gracefulShutdown("SIGTERM"));
+  process.on("SIGINT", () => gracefulShutdown("SIGINT"));
+
+  process.on("uncaughtException", (error) => {
+    console.error("💥 Uncaught Exception:", error);
+    gracefulShutdown("uncaughtException");
+  });
+
+  process.on("unhandledRejection", (reason) => {
+    console.error("💥 Unhandled Rejection:", reason);
+    gracefulShutdown("unhandledRejection");
+  });
+}
+
+// Start the application
+startServer();

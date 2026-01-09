7f7543b8ef217e60d3a6b7ad4e14c627c6d890db
Toha Hossain <tohahpro@gmail.com>
2026-01-09 11:54:01 +0600

    Schedule get method admin permission setup

--- src/app/modules/schedule/schedule.route.ts
+++ src/app/modules/schedule/schedule.route.ts
@@ -6,7 +6,7 @@ import { UserRole } from "@prisma/client";
 const router = express.Router();
 
 router.post("/", ScheduleController.insertIntoDB)
-router.get("/", auth(UserRole.Doctor), ScheduleController.schedulesForDoctor)
+router.get("/", auth(UserRole.Doctor, UserRole.Admin), ScheduleController.schedulesForDoctor)
 router.delete("/:id", ScheduleController.deleteScheduleFromDB)
 
 export const ScheduleRoutes = router;
